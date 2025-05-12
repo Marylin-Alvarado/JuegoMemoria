84e57b55537bd969b382109e3d996972588c0baf
Requisitos Marylin  Alvarado <marylin.alvarado@unl.edu.ec>
2025-05-12 18:04:26 -0500

    version 2 agregar img

--- card.py
+++ card.py
@@ -1,16 +1,25 @@
-class Card:
-    def __init__(self, name):
-        self.name = name
-        self.revealed = False
+from PIL import Image, ImageTk
+import tkinter as tk
 
-    def reveal(self):
-        self.revealed = True
+class Card:
+    def __init__(self, master, image_path, command):
+        self.image_path = image_path
+        self.image = Image.open(image_path)
+        self.image = self.image.resize((100, 100))
+        self.photo = ImageTk.PhotoImage(self.image)
+        self.button = tk.Button(master, image=self.photo, command=command)
+        self.is_flipped = False
+        self.is_matched = False
+
+    def flip(self):
+        if not self.is_flipped and not self.is_matched:
+            self.button.config(image=self.photo)
+            self.is_flipped = True
 
     def hide(self):
-        self.revealed = False
+        if not self.is_matched:
+            self.button.config(image=self.back_image)
+            self.is_flipped = False
 
-    def __repr__(self):
-        return self.name if self.revealed else "🂠"
-
-    def __str__(self):
-        return self.name
+    def match(self):
+        self.is_matched = True
--- game_logic.py
+++ game_logic.py
@@ -1,18 +1,53 @@
-from board import Board
+import tkinter as tk
+import random
+from card import Card
+from PIL import Image, ImageTk
+import os
 
 class MemoryGame:
-    def __init__(self, theme):
-        self.board = Board(theme)
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Juego de Memoria: Flores y Animales")
+        self.cards = []
+        self.first_card = None
+        self.load_images()
+        self.create_board()
 
-    def play_turn(self, idx1, idx2):
-        card1 = self.board.reveal_card(idx1)
-        card2 = self.board.reveal_card(idx2)
-        self.board.display()
-        if card1.name != card2.name:
-            print("¡No coinciden!")
-            self.board.hide_cards(idx1, idx2)
+    def load_images(self):
+        self.images = []
+        assets_path = os.path.join(os.getcwd(), 'assets')
+        for file in os.listdir(assets_path):
+            if file != 'reverso.png':
+                self.images.append(os.path.join(assets_path, file))
+        self.images *= 2  # Duplicar imágenes para pares
+        random.shuffle(self.images)
+        self.back_image = Image.open(os.path.join(assets_path, 'reverso.png'))
+        self.back_image = self.back_image.resize((100, 100))
+        self.back_photo = ImageTk.PhotoImage(self.back_image)
+
+    def create_board(self):
+        for index, image_path in enumerate(self.images):
+            card = Card(self.master, image_path, lambda c=index: self.on_card_click(c))
+            card.button.grid(row=index // 4, column=index % 4)
+            card.back_image = self.back_photo
+            card.button.config(image=self.back_photo)
+            self.cards.append(card)
+
+    def on_card_click(self, index):
+        card = self.cards[index]
+        if card.is_flipped or card.is_matched:
+            return
+        card.flip()
+        if not self.first_card:
+            self.first_card = card
         else:
-            print("¡Pareja encontrada!")
+            self.master.after(1000, self.check_match, card)
 
-    def is_finished(self):
-        return self.board.is_complete()
+    def check_match(self, second_card):
+        if self.first_card.image_path == second_card.image_path:
+            self.first_card.match()
+            second_card.match()
+        else:
+            self.first_card.hide()
+            second_card.hide()
+        self.first_card = None
